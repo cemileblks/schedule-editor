787c2ab148088971ac952598e8441c03bf8d3b8a
Cemileb <cemilebalkas@gmail.com>
2024-03-25 21:48:21 +0000

    added DOM elements to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,11 @@ mySchedule.addOperation(OperationTemplates.transferPlate("Plate1", true, "Lab1",
 mySchedule.addOperation(OperationTemplates.deviceOperation("DispenseReagent", "Bioraptr", "plate"));
 
 mySchedule.printOperations();
+
+const createButton = document.getElementById("create-schedule-btn");
+const deleteButton = document.getElementById("delete-schedule-btn");
+const scheduleFilter = document.getElementById("schedule-filter");
+const infoButton = document.getElementById("info-btn");
+const schedulesList = document.getElementById("schedules-list");
+const openedSchedule = document.getElementsByClassName("opened-schedules");
+const operationsContainer = document.getElementsByClassName("schedule-operation-container");
